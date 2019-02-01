ac2df99de4d02270bd3e369fe3e5302ea00c5734
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-02-01 22:15:59 +0100

    Moved clients component to only one card

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent {
       return [
         { title: 'Card 1', cols: 2, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Clients', cols: 1, rows: 2 },
         { title: 'Card 4', cols: 1, rows: 1 }
       ];
     })
